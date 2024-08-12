fcf2657de7b8afef9d5c60ca9ad692459a71df2e
Yuli <stremovsky@gmail.com>
2024-08-12 19:54:19 +0300

    add script

---
+++ static/api/aws-redirect.php
@@ -0,0 +1,4 @@
+<?php
+#header("Location: https://aws.amazon.com/marketplace/seller-profile?id=seller-uf6lp4f7635lq");
+#header("Location: https://databunker.org/");
+header("Location: https://databunker.org/contact/");
