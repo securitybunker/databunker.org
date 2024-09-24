1436cfe282983dd8d9b1ed70a98d796196bb99c7
Yuli <stremovsky@gmail.com>
2024-09-24 23:56:17 +0300

    save

--- static/api/aws-redirect.php
+++ static/api/aws-redirect.php
@@ -1,4 +1,4 @@
 <?php
 #header("Location: https://aws.amazon.com/marketplace/seller-profile?id=seller-uf6lp4f7635lq");
 #header("Location: https://databunker.org/");
-header("Location: https://databunker.org/contact/");
+header("Location: https://databunker.org/databunker-pro/");
--- static/api/meeting.php
+++ static/api/meeting.php
@@ -1,29 +1,4 @@
 <?php
 
-#@require_once('databunker-api.php');
-@require_once('/var/www/privacybunker.io/html/api/databunker-api.php');
+header("Location: https://calendly.com/stremovsky/30min");
 
-$id = @$_GET['id'];
-
-$p = "";
-if (!empty($_POST)) {
-  $p = json_encode(@$_POST);
-  if ($p) {
-    @error_log("meeting post $p");
-  }
-  $email = get_string('email', 'post');
-  $email = strtolower($email);
-  $name = get_string('name', 'post');
-  $phone = get_string('phone', 'post');
-  $company = get_string('company', 'post');
-  $country = get_string('country', 'post');
-  $hostname = get_string('hostname', 'post');
-  $topics = get_string('topics', 'post');
-  if ($email) {
-    send_book_demo($topics, $name, $phone, $email, $hostname);
-  }
-  #if (!empty($email)) {
-  #  update_user_by_post($email);
-  #}
-}
-print("ok");
